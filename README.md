3a44bba75da6ad7ae75102983c962afba73aa224
aoleson <alannah313@gmail.com>
2024-12-10 14:38:04 -0700

    Major site update: New template, reduced styling overhead, bootstrap integration, flattened structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# personal-website
-Alannah's personal website and related materials.
+Alannah Oleson's personal website. 
+
+Based on the [plain-academic template](https://github.com/mavroudisv/plain-academic) by Vasilios Mavroudis. 
--- js/reloadImage.js
+++
@@ -1,8 +0,0 @@
-function reloadImage() {
-    if (document.images) {
-        var now = new Date();
-        document.images.randimg.src = “/randimg?” + now.getTime();
-        setTimeout(“reloadImage()”, 5000);
-    }
-}
-setTimeout(“reloadImage()”, 5000);
